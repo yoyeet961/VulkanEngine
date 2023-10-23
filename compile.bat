f8666c011434f02390be4d3b182dc96ad6095b77
yoyeet961 <oxana.rw@gmail.com>
2023-10-23 20:49:58 +0300

    Sync

---
+++ compile.bat
@@ -0,0 +1,3 @@
+E:\Programs\VulkanSDK\1.3.261.1\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+E:\Programs\VulkanSDK\1.3.261.1\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+pause
